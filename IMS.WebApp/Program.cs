5fcb46a81687ccd53e2037271ef02a8ab64b3918
Gleidson Guilhem <gleidson.guilhem@gmail.com>
2026-01-11 15:45:11 +1100

    add folder control to create resuable components
    Create InventoryListComponent

--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -11,7 +11,6 @@ builder.Services.AddRazorComponents();
 builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
 builder.Services.AddTransient<IViewInventoryByNameUseCase, ViewInventoryByNameUseCase>();
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
