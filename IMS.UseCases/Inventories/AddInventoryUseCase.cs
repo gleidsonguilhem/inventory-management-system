9ffb7881ecbc8d6005b6a93eb8efb197fa93cff3
Gleidson Guilhem <gleidson.guilhem@gmail.com>
2026-01-12 12:18:41 +1100

    WebAPI - break InventoryListComponent into another component InventoryListItemComponent to separate responsibility
    UseCases - Create AddInventoryUseCase class and IAddUseCase Interface

---
+++ IMS.UseCases/Inventories/AddInventoryUseCase.cs
@@ -0,0 +1,24 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.UseCases.Inventories
+{
+    internal class AddInventoryUseCase : IAddInventoryUseCase
+    {
+        private IInventoryRepository inventoryRepository;
+
+        public AddInventoryUseCase(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+
+        }
+        public async Task ExecuteAsync(Inventory inventory)
+        {
+            await this.inventoryRepository.AddInventoryAsync(inventory);
+        }
+
+    }
+}
